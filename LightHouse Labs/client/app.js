8456d6d60f6c882bb7e2c0d9cf25f593ed7a1193
Sean Park <sean.park47@gmail.com>
2017-11-29 22:36:21 -0800

    Add files via upload

---
+++ LightHouse Labs/client/app.js
@@ -0,0 +1,13 @@
+var socket = io();
+
+$('form').submit(function () {
+  var text = $('#initials').val() + " says: " + $('#message').val();
+  socket.emit('message', text);
+  $('#message').val('');
+  return false;
+});
+
+socket.on('message', function (msg) {
+  $('<li>').text(msg).appendTo('#history');
+  pastMessages.push('message')
+});
